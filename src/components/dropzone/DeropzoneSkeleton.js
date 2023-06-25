b26fb2689e199c26b53d1785866113d6d2ebd0e7
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-06-25 19:28:56 +0530

    loaders added

--- src/components/dropzone/DeropzoneSkeleton.js
+++ src/components/dropzone/DeropzoneSkeleton.js
@@ -4,8 +4,8 @@ import React from "react";
 function DeropzoneSkeleton() {
   return (
     <Box pb={10} pt={5}>
-      <Skeleton w={"100%"} h={"80px"} count={1} baseColor="#fffefc" />
-      <Skeleton my={5} count={1} w={"100%"} h={"30px"} baseColor="#fffefc" />
+      <Skeleton w={"100%"} h={"80px"} count={1} baseColor="#fffffc" />
+      <Skeleton my={5} count={1} w={"100%"} h={"30px"} baseColor="#fffffc" />
     </Box>
   );
 }
